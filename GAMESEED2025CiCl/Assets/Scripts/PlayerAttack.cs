aa6211ec684f9e7444307b08938c9cd213cf7347
Alvin Ababil <neoryumasil@gmail.com>
2025-07-28 18:01:46 +0800

    Update PlayerAttack.cs
    Menambahkan attack mengggunakan WASD dan onBeat

--- GAMESEED2025CiCl/Assets/Scripts/PlayerAttack.cs
+++ GAMESEED2025CiCl/Assets/Scripts/PlayerAttack.cs
@@ -4,36 +4,84 @@ public class PlayerAttack : MonoBehaviour
 {
     public GameObject aoePrefab;
     public float aoeRadius = 7.0f;
-    private float aoeDuration = 3.0f;
-    private float nextAttack = 0.0f;
+    public float beatInterval = 0.5f;
+    public float beatWindow = 0.15f;
+
+    private float attackCooldownHoreg1 = 1.0f;
+    private float attackCooldownHoreg2 = 3.0f;
+    private float nextBeatTime = 0.0f;
+    private float nextAttackHoreg1 = 0.0f;
+    private float nextAttackHoreg2 = 0.0f;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        nextBeatTime = Time.time * beatInterval;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Tekan Spasi untuk Attack jika tidak cooldown
-        if (Input.GetKeyDown(KeyCode.Space) & Time.time >= nextAttack)
+        Metronome();
+
+        // Horeg 1
+        if ((Input.GetKeyDown(KeyCode.A)|| Input.GetKeyDown(KeyCode.D)) && Time.time >= nextAttackHoreg1)
         {
-            TriggerAOE();
-            nextAttack = Time.time + aoeDuration;
+            TriggerAOE("Horeg1", attackCooldownHoreg1);
+            nextAttackHoreg1 = Time.time + attackCooldownHoreg1;
+        }
+
+        // Horeg 2
+        if ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S)) && Time.time >= nextAttackHoreg2)
+        {
+            TriggerAOE("Horeg2", attackCooldownHoreg2);
+            nextAttackHoreg2 = Time.time + attackCooldownHoreg2;
         }
     }
 
     // Untuk Trigger AOE
-    void TriggerAOE()
+    void TriggerAOE(string horeg, float duration)
     {
+        bool isOnBeat = Mathf.Abs(Time.time - nextBeatTime + beatInterval) <= beatWindow;
+
         // Instanstiate AOE
         GameObject aoeInstance = Instantiate(aoePrefab, transform.position, Quaternion.identity);
 
         // AOE Radius
         aoeInstance.transform.localScale = new Vector3(aoeRadius, 0.1f, aoeRadius);
 
+        // Kalau On Beat (Dugem)
+        Renderer aoeRenderer = aoeInstance.GetComponent<Renderer>();
+
+        if (isOnBeat)
+        {
+            aoeRenderer.material.color = Color.gold;
+            Debug.Log($"{horeg} On Beat Dapat Bonus");
+        }
+        else
+        {
+            if (horeg == "Horeg1")
+            {
+                aoeRenderer.material.color = Color.red;
+            } 
+            else if (horeg == "Horeg2")
+            {
+                aoeRenderer.material.color = Color.blue;
+            }
+
+            Debug.Log($"{horeg} Normal Attack");
+        }
+
         // Destroy AOE
-        Destroy(aoeInstance, aoeDuration);
+        Destroy(aoeInstance, duration);
+    }
+
+    void Metronome()
+    {
+        if (Time.time >= nextBeatTime)
+        {
+            nextBeatTime += beatInterval;
+        }
     }
 }

86ae26e72d47f3e9e75fd69cae4dca14150a53e4
elderaka <laudadraka@gmail.com>
2025-08-03 16:33:19 +0700

    Fucked around so I need to revert

--- GAMESEED2025CiCl/Assets/Scripts/UI/LoadingScene.cs
+++ GAMESEED2025CiCl/Assets/Scripts/UI/LoadingScene.cs
@@ -17,16 +17,29 @@ public class LoadingScene : MonoBehaviour
     IEnumerator LoadSceneAsync(int sceneId)
     {
         AsyncOperation operation = SceneManager.LoadSceneAsync(sceneId);
+        // Jadi sebelum scenenya load, jangan diaktifin dulu
+        operation.allowSceneActivation = false; 
 
         LoadingScreen.SetActive(true);
 
-        while (true)
+        // Loadingnya ampe 90% loaded aja
+        while (operation.progress < 0.9f)
         {
             float progressValue = Mathf.Clamp01(operation.progress / 0.9f);
 
             LoadingBarFill.fillAmount = progressValue;
 
+            Debug.Log($"Loading... {Mathf.RoundToInt(progressValue * 100)}%");
+
             yield return null;
         }
+
+        Debug.Log("Loading... 100%");
+        LoadingBarFill.fillAmount = 1f; 
+
+        yield return new WaitForSeconds(0.5f);
+
+        Debug.Log("Done loading. Activating scene.");
+        operation.allowSceneActivation = true; 
     }
 }
